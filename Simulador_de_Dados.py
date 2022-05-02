df80fd4cdcda0e28abf65138987baa34df770ebe
CLAUDEMIR MARTINS FERRAZ <claudemirmferraz@gmail.com>
2022-05-01 22:17:10 -0300

    geração de tela

--- Simulador_de_Dados.py
+++ Simulador_de_Dados.py
@@ -9,28 +9,29 @@ class SimuladorDeDado:
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
-        self.mensagem = 'Voce gostaria de jogar o dado?'
 
         #Layout
-        layout = [
+        self.layout = [
             [sg.Text('Jogar o dado?')],
             [sg.Button ('sim'),sg.Button('não')]
         ]
+    def Iniciar(self): 
         #Criar uma janela
-        #Ler os valores na tela
-        #Realizar algo com os valores extraidos
+        self.janela = sg.Window ('Simulador de Dado', layout= self.layout)
 
-    def Iniciar(self):
-        resposta = input(self.mensagem)
-        try:
-            if resposta == 'sim' or resposta == 's':
-                self.GerarValorDoDado()
-            elif resposta == 'não' or resposta =='n':
-                print('Agradeço a sua participação')
-            else:
-                print('Favor digitar sim ou o não')
-        except:
-                print('Ocorreu um erro ao receber sua reposta!!')
+        #Ler os valores na tela
+        self.eventos, self.valores = self.janela.Read()
+
+        #Realizar algo com os valores extraidos
+            try:
+                if self.evento == 'sim' or self.evento == 's':
+                    self.GerarValorDoDado()
+                elif self.evento == 'não' or self.evento =='n':
+                    print('Agradeço a sua participação!!')
+                else:
+                     print('Favor digitar sim ou o não')
+            except:
+                    print('Ocorreu um erro ao receber sua resposta!!')
 
     def GerarValorDoDado(self):
         print(random.randint(self.valor_minimo, self.valor_maximo))
